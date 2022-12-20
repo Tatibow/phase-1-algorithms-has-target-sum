ef0a0d1aeac348ee9b7af9ce554cfb3d5316dbbc
Tatiana <Bowmantatiana2@gmail.com>
2022-12-19 17:48:17 -0800

    finished sol

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    let loop1 = array[i];
+    for(let j = i + 1; j < array.length; j++) {
+      let loop2 = array[j]
+      if(loop1 + loop2 === target) return true;
+    }
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
-/* 
-  Add your pseudocode here
+/*
+  The function should return true if atleast two numbers in the array
+  adds up to the target.
 */
 
 /*
   Add written explanation of your solution here
+  I used what's called a nested loop to help me solve the problem. The first loop starts at index 0 and the second starts at index 1
+  so as it goes through the array it can search for the sum in pairs.
 */
 
 // You can run `node index.js` to view these console logs
